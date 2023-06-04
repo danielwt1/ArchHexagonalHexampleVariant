d44ed44879b37f231613beed64b7c0ee3dd287c5
Daniel Ramos <dramosg@ucentral.edu.co>
2023-06-03 19:17:28 -0500

    first commit

---
+++ src/main/java/com/example/archhexagonalvariant/ArchHexagonalVariantApplication.java
@@ -0,0 +1,13 @@
+package com.example.archhexagonalvariant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchHexagonalVariantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchHexagonalVariantApplication.class, args);
+    }
+
+}
